22229017a4acb1e78271e40895942994345b76d4
Andrew Lloyd <andrew85.lloyd@gmail.com>
2018-11-12 21:38:15 -0800

    use index of to find transcript <p>

--- server/app/SEDTranscriptIngester.js
+++ server/app/SEDTranscriptIngester.js
@@ -49,11 +49,10 @@ function getPostTranscriptUrl(postUrl) {
     const dom = new JSDOM(response);
     let list = dom.window.document.getElementsByClassName("post__content")[0].getElementsByTagName("p")
     for (var i = 0; i < list.length; i++) {
-      // if (list[i].innerHTML contains "Transcript provided by We Edit Podcasts") {
-      //   console.log(list[i])
-      // }
+      if (list[i].innerHTML.indexOf("Transcript provided by We Edit Podcasts") > -1) {
+        console.log(list[i].innerHTML)
+      }
     }
-    
   })
 }
 
